0f0d1ab0437402ae5668109b230fe9db22b6ed30
O.Surnin <legiks@gmail.com>
2017-11-21 10:55:04 +0300

    [DB] connection cfg

---
+++ sql.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import cfg
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = cfg.connection
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    alias = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    cluster = db.Column(db.Integer, db.ForeignKey('cluster.id'),
+                        nullable=False)
+    files = db.relationship('File', backref='owner', lazy='dynamic')
+    # TODO: add size constrain
+    size = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.alias
+
+
+class Cluster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    main = db.Column(db.Integer, db.ForeignKey('server.id'), nullable=False)
+    second1 = db.Column(db.Integer, db.ForeignKey('server.id'), nullable=False)
+    second2 = db.Column(db.Integer, db.ForeignKey('server.id'), nullable=False)
+    users = db.relationship('User', backref='tenant', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Cluster %r>' % self.id
+
+
+class Server(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.BOOLEAN, nullable=False)
+    address = db.Column(db.String(45), nullable=False)
+    main_clusters = db.relationship('Cluster', backref='mains', lazy='dynamic',
+                                    foreign_keys=[Cluster.main])
+    second1_clusters = db.relationship('Cluster', backref='seconds1',
+                                       lazy='dynamic',
+                                       foreign_keys=[Cluster.second1])
+    second2_clusters = db.relationship('Cluster', backref='seconds2',
+                                       lazy='dynamic',
+                                       foreign_keys=[Cluster.second2])
+
+    def __repr__(self):
+        return '<Server %r>' % self.id
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(125), unique=True, nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<File %r>' % self.name
